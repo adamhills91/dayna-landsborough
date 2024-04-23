2b6efe2ebe23141b2cd94353add9993305418d35
Adam Hills <adam@hamblyfreeman.com>
2024-04-23 18:56:05 +0100

    Form clears on submit

--- src/js/form.js
+++ src/js/form.js
@@ -9,15 +9,14 @@ const handleSubmit = (event) => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(
-      () =>
-        (document.querySelector(".form-submit-message").textContent =
-          "Thank you for your message, I'll respond as soon as possible.")
-    )
-    .catch(
-      (error) =>
-        (document.querySelector(".form-submit-message").textContent = error)
-    );
+    .then(() => {
+      document.querySelector(".form-submit-message").textContent =
+        "Thank you for your message, I'll respond as soon as possible.";
+      document.querySelector("form").reset();
+    })
+    .catch((error) => {
+      document.querySelector(".form-submit-message").textContent = error;
+    });
 };
 
 document.querySelector("form").addEventListener("submit", handleSubmit);
