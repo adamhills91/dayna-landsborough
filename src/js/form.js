755cee4667e82ef81714d09ff30424e5ef9b4d5d
Adam Hills <adam@hamblyfreeman.com>
2024-04-23 18:52:05 +0100

    Edited form handler to show send message

--- src/js/form.js
+++ src/js/form.js
@@ -9,8 +9,15 @@ const handleSubmit = (event) => {
     headers: { "Content-Type": "application/x-www-form-urlencoded" },
     body: new URLSearchParams(formData).toString(),
   })
-    .then(() => alert("Thank you for your submission"))
-    .catch((error) => alert(error));
+    .then(
+      () =>
+        (document.querySelector(".form-submit-message").textContent =
+          "Thank you for your message, I'll respond as soon as possible.")
+    )
+    .catch(
+      (error) =>
+        (document.querySelector(".form-submit-message").textContent = error)
+    );
 };
 
 document.querySelector("form").addEventListener("submit", handleSubmit);
